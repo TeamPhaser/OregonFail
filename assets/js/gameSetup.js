baad64fa7b40ec36deb7927402fb6b9bb8d2a6be
Jordan Campbell <jordan@ignitionstudios.net>
2016-06-30 19:31:40 -0500

    Transition finished

--- assets/js/gameSetup.js
+++ assets/js/gameSetup.js
@@ -5,13 +5,37 @@ var GameSetup = {
 	},
 
 	create: function() {
+		// create background image to game
 		background = game.add.sprite(0, 0, 'background');
+		// scale background to fit game screen
 		background.scale.setTo(2.79);
 
+		// Styling stuff!
 		var style = { font: "32px Arial", fill: "black", align: "center", backgroundColor: "#eef" };
-		text = game.add.text(10, 50, "Welcome to the Oregon Trail", style);
+		// create text 
+		text = game.add.text(30, 0, "Welcome to the Oregon Trail", style);
+		text.alpha = 0;
+		text.scale.x = .4;
+		text.scale.y = .4;
 
+		// Create black overlay
 		black = game.add.sprite(0, 0, 'black');
-		game.add.tween(black).to({alpha: 0}, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
+		// Fade the black overlay out
+		game.add.tween(black).to({alpha: 0}, 2000, 'Linear', true, 0);
+
+		setTimeout(function(){
+			// Animating movement
+			game.add.tween(text).to({
+				x: 10,
+				y: 50,
+				alpha: 1,
+			}, 2000, 'Linear', true, 0);
+
+			// Animating scale
+			game.add.tween(text.scale).to({
+				x: 1,
+				y: 1,
+			}, 2000, 'Linear', true, 0);
+		}, 1500);
 	}
 }
