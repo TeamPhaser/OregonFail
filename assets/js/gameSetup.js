138cc250e025fb0f657bbaf3f49b4b3720e9f772
Kenneth Torng <ktorng@gmail.com>
2016-06-30 19:51:49 -0500

    added name and companion inputs on gameSetup

--- assets/js/gameSetup.js
+++ assets/js/gameSetup.js
@@ -5,6 +5,9 @@ var GameSetup = {
 	},
 
 	create: function() {
+    // plugin for text-input fields in canvas
+    game.add.plugin(Fabrique.Plugins.InputField);
+
 		// create background image to game
 		background = game.add.sprite(0, 0, 'background');
 		// scale background to fit game screen
@@ -37,5 +40,27 @@ var GameSetup = {
 				y: 1,
 			}, 2000, 'Linear', true, 0);
 		}, 1500);
+
+    var nameInput = textInput(40, 200, 'Enter your name');
+    var companion1 = textInput(40, 250, 'Enter companion 1');
+    var companion2 = textInput(40, 280, 'Enter companion 2');
+    var companion3 = textInput(40, 310, 'Enter companion 3');
+    var companion4 = textInput(40, 340, 'Enter companion 4');
 	}
+}
+
+// helper function for creating text inputs on canvas
+function textInput(x, y, placeholder) {
+  return game.add.inputField(x, y, {
+    font: '18px Arial',
+    fill: '#212121',
+    fontWeight: 'bold',
+    width: 200,
+    height: 10,
+    padding: 5,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 6,
+    placeHolder: placeholder
+  });
 }
